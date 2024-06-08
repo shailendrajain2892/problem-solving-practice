a194ff20f99f5163dab7dc21e2edb8e233999d45
Shailendra Jain <shailendrajain28@gmail.com>
2024-06-08 10:06:59 +0100

    reverse a linked list

---
+++ linkedlist/doubly_linked_list.py
@@ -0,0 +1,20 @@
+class Node:
+    def __init__(self, value) -> None:
+        self.data = value
+        self.prev = None
+        self.next = None
+
+class LinkedList:
+    def __init__(self) -> None:
+        self.head = None
+
+    def insert(self): 
+        pass
+
+    def delete(self):
+        pass
+
+    def search(self):
+        pass
+
+    
--- linkedlist/linkedlist.py
+++ linkedlist/linkedlist.py
@@ -1,3 +1,6 @@
+import sys
+
+
 class Node:
     def __init__(self, data) -> None:
         self.data = data
@@ -12,17 +15,18 @@ class LinkedList:
         while(tmp!=None):
             print(tmp.data, end=",")
             tmp=tmp.next
-
+        print(end="\n")
+        
     def insertAtEnd(self, value) -> None:
         node = Node(value)
         if self.head == None:
-            print(f"head is none now, so putting {node.data} at the beginning")
+            # print(f"head is none now, so putting {node.data} at the beginning")
             self.head = node
         else:
             tmp = self.head
             while(tmp.next!=None):
                 tmp = tmp.next
-            print(f"inserting {node.data} at the end")
+            # print(f"inserting {node.data} at the end")
             tmp.next = node
 
     def insertAtBeg(self, value) -> None: 
@@ -46,14 +50,71 @@ class LinkedList:
                     tmp.next = node
                 tmp=tmp.next
         
-                
-                
+    def search(self,value) -> bool:
+        if self.head == None:
+            return False
+        tmp = self.head
+        while(tmp!=None):
+            if tmp.data == value:
+                print(f"found : {value}")
+                return True
+            tmp=tmp.next
+        print(f"{value} not found")
+        return False
+    
+    def find_n_element_from_end(self, k) -> int:
+        slow=self.head
+        fast=self.head
+        for _ in range(k):
+            fast=fast.next
+        while(fast!=None):
+            slow=slow.next
+            fast=fast.next
+        return slow.data
+    
+    def detect_loop_change_ref(self) -> bool: #approach 1
+        tmp = Node(0)
+        curr = self.head
+        while(curr!=None and curr.next!=tmp):
+            tmp2=curr.next
+            curr.next=tmp
+            curr=tmp2
+        if curr==None:
+            return False
+        return True
+    
+    def detect_loop_floyd_cycle(self) -> bool:
+        slow=self.head
+        fast=self.head
+        while(fast!=None and fast.next!=None):
+            slow=slow.next
+            fast=fast.next.next
+            if slow==fast:
+                return True
+        return False
+    
+    def reverse(self) -> None:
+        curr = self.head
+        prev=None
+        tmp=None
+        while(curr != None):
+            tmp=curr.next
+            curr.next=prev
+            prev=curr
+            curr=tmp
+        self.head=prev
+
 
 numbers = [5,8,9,12,53]
 ll = LinkedList()
 [ll.insertAtEnd(n) for n in numbers]
    
 
-ll.insertAtK(10,9)
+# ll.insertAtK(10,9)
 ll.traverse()
-        
+# ll.search(1)
+# k=int(sys.argv[1])
+# print(f"2nd element from end = {ll.find_n_element_from_end(k)}")
+print(f"Reverse a linked list")
+ll.reverse()
+ll.traverse()
